6a4a7793255a00c76d369cb0b4f0f255f8b987d6
ZibaloV <fotoroom.md@gmail.com>
2019-09-29 07:29:37 +0300

    routes be

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars')
 
 const app = express();
+
+app.use(express.static('public'));
+
+var hbs = exphbs.create({
+    /* config */
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+// Register `hbs.engine` with the Express app.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views')
+/* routing________________________________ */
+
+app.get('/', (req, res) => {
+    res.render('index', {
+            title: "home",
+            isHome: true
+        }
+
+    );
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/component', (req, res) => {
+    res.render('component', {
+        title: "component",
+        isComponent: true
+    });
+})
+
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: "ADD",
+        isAdd: true
+    });
+})
+
+app.get('*', (req, res) => {
+    res.render('404');
+})
+
+
 
 const PORT = process.env.PORT || 3000;
 
