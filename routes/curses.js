140e77226182f729e7cfce5ccad9f2eef2465c67
ZibaloV <fotoroom.md@gmail.com>
2019-10-01 05:36:12 +0300

    connect  clients scripts

--- routes/curses.js
+++ routes/curses.js
@@ -1,14 +1,19 @@
 const {
     Router
 } = require('express');
+const Course = require('../models/course');
 const router = Router();
+const course = new Course(); // в референсе не создаётся новый обьект
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const courses = await course.getAll();
     res.render('curses', {
         title: "curses",
-        isComponent: true
+        isComponent: true,
+        courses // так предаётся переменная
     });
 })
+
 
 
 
