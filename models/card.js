a3ce13b2a71c98c656fd0fc701baa4a8fea1dc0c
ZibaloV <fotoroom.md@gmail.com>
2019-10-04 09:54:53 +0300

    end lesson15

--- models/card.js
+++ models/card.js
@@ -1,12 +1,57 @@
 const fs = require('fs');
 const path = require('path');
 
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'card.json'
+);
+
 class Card {
-    add() {
 
+    static async add(course) {
+        console.log('course__', course);
+
+        const card = await Card.fetch();
+        console.log("card__", card);
+
+        const idx = card.courses.findIndex(c => c.id === course.id);
+        console.log("idx", idx);
+        const candidate = card.courses[idx]; // ?? -1 если не находит 
+        if (candidate) {
+            console.log("курс уже есть");
+            card.courses[idx].count++;
+        } else {
+            // нужно добавить
+            course['count'] = 1;
+            card.courses.push(course);
+        }
+
+        card.price += +course.price;
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), (err) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve()
+                }
+            })
+
+        })
     }
-    fetch() {
 
+    static fetch() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(p, 'utf-8', (err, content) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(content));
+                }
+            })
+        })
     }
 
 }
--- routes/card.js
+++ routes/card.js
@@ -6,6 +6,7 @@ const Course = require('../models/course');
 const router = Router();
 
 router.post('/add', async (req, res) => {
+    console.log("req.body.id", req.body.id);
     const course = await Course.getById(req.body.id);
     await Card.add(course);
     res.redirect('/card');
@@ -15,7 +16,8 @@ router.get('/', async (req, res) => {
     const card = await Card.fetch();
     res.render('card', {
         title: 'корзина',
-        card
+        courses: card.courses,
+        price: card.price
     })
 })
 
