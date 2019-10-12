04a45077ebf38808000779ef29a2ac5656d5f7f1
ZibaloV <fotoroom.md@gmail.com>
2019-10-12 08:12:58 +0300

    end less13 block4

--- routes/orders.js
+++ routes/orders.js
@@ -6,14 +6,61 @@ const router = Router();
 
 
 router.get('/', async (req, res) => {
-    res.render('orders', {
-        isOrder: true,
-        title: "заказы"
-    })
+    try {
+        const orders = await Order.find({
+                'user.userid': req.user._id
+            })
+            .populate('userId')
+
+        res.render('orders', {
+            isOrder: true,
+            title: "заказы",
+            orders: orders.map(o => {
+                return {
+                    ...o._doc,
+                    price: o.courses.reduce((total, c) => {
+                        return total += c.count * c.course.price
+                    }, 0)
+                }
+            })
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
-router.post('/', (req, res) => {
-    res.redirect('/orders');
+router.post('/', async (req, res) => {
+    try {
+        const user = await req.user
+            .populate('cart.items.courseId')
+            .execPopulate()
+        //console.log('user', user);
+        const courses = user.cart.items.map(i => ({
+            count: i.count,
+            course: {
+                ...i.courseId._doc
+            }
+        }))
+        //console.log('courses', courses);
+
+        const order = new Order({
+            user: {
+                name: req.user.name,
+                userId: req.user
+            },
+            courses: courses
+        })
+
+        await order.save();
+        await req.user.clearCart();
+
+        res.redirect('/orders');
+    } catch (error) {
+        console.log('не очистилась');
+        console.log(error);
+    }
+
 })
 
 
