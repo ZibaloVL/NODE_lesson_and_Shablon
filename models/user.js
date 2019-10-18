bbfcd80176da31659919a27a46a81c6f679b661b
ZibaloV <fotoroom.md@gmail.com>
2019-10-18 13:46:12 +0300

    end lesson7 block5

--- models/user.js
+++ models/user.js
@@ -10,6 +10,10 @@ const userSchema = new Schema({
     },
     name: {
         type: String,
+
+    },
+    password: {
+        type: String,
         required: true
     },
     cart: {
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
-const {Router} = require('express');
-const User =require('../models/user');
+const {
+    Router
+} = require('express');
+const User = require('../models/user');
 router = Router();
 
-router.get('/login', async(req,res) => {
+router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'авторизация',
         isLogin: true
@@ -12,21 +14,56 @@ router.get('/login', async(req,res) => {
 router.get('/logout', async (req, res) => {
     req.session.destroy(() => { // callback active after cleanin session
         res.redirect('/auth/login#login');
-    }) 
+    })
 })
 
-router.post('/login', async (req, res)=>{
+router.post('/login', async (req, res) => {
     const user = await User.findById('5d9c8f0822388d08a851f5b5');
     req.session.user = user;
     req.session.isAuthenticated = true;
     req.session.save(err => { // method for awaiting get from db param user!!!!
-        if(err){
+        if (err) {
             console.log('err connect');
             throw err;
         }
         res.redirect('/');
     })
     console.log('req.session', req.session);
+})
+
+router.post('/register', async (req, res) => {
+    try {
+        console.log('req.body', req.body);
+        const {
+            email,
+            password,
+            repeat,
+            name
+        } = req.body;
+        const candidate = await User.findOne({
+            email
+        });
+
+        if (candidate) {
+            // console.log('/auth/login#register');
+            res.redirect('/auth/login#register');
+        } else {
+            const user = new User({
+                email,
+                name,
+                password,
+                cart: {
+                    'items': []
+                }
+            });
+            await user.save();
+            // console.log('/auth/login#login');
+            res.redirect('/auth/login#login');
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
