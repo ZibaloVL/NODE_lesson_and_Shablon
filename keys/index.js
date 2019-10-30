b7c24cb9608c7e1637df4f347906751b3b28c3e3
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2019-10-30 05:48:29 +0200

    end les2 block6

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URI : `mongodb+srv://fotoroom:lcROiGz73NlNKrSW@nodeshoplearn-fif3b.gcp.mongodb.net/shop`,
-    SESSION_SECRET:  'some secret value'
+    SESSION_SECRET:  'some secret value',
+    SENDGRID_API_KEY:'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'
 }
