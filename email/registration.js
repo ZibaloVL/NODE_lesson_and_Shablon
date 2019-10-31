9e6d92a019dcee75469c19d7775426f1cc29a9e1
ZibaloV <fotoroom.md@gmail.com>
2019-10-31 05:45:56 +0200

    not send  email

--- email/registration.js
+++ email/registration.js
@@ -1,14 +1,10 @@
 const keys = require('../keys/index.js');
- 
+
 module.exports = function (email) {
     return {
-        to: email ,
-        from: keys.EMAIL_FROM,
+        to: email,
+        from: 'sweetsons22@gmail.com',
         subject: 'Акаунд создан',
-        html: `
-            <h1> welcome in course</h1>
-            <a href=${keys.BASE_URL}> НА ГЛАВНУЮ</a>
-        `
-
+        html: `<h1> welcome in course</h1>`
     }
 }
