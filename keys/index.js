08e17882129124eac0f07503f74238bdda62892e
ZibaloV <fotoroom.md@gmail.com>
2019-10-31 07:09:05 +0200

    change ignor

--- keys/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    MONGODB_URI : `mongodb+srv://fotoroom:lcROiGz73NlNKrSW@nodeshoplearn-fif3b.gcp.mongodb.net/shop`,
-    SESSION_SECRET:  'some secret value',
-    SENDGRID_API_KEY:,
-    EMAIL_FROM: 'node@curse.js',
-    BASE_URL:  'http://localhost:3000'  
-}
