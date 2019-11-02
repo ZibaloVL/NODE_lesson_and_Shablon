7f1d09755d505a844bfee42ffa1add586c0bc701
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2019-11-02 10:48:12 +0200

    end block7

--- routes/profile.js
+++ routes/profile.js
@@ -2,10 +2,12 @@ const {
     Router
 } = require('express');
 const auth = require('../middleware/auth');
+const User = require('../models/user');
 const router = Router();
 
 
 router.get('/', async (req, res) => {
+    console.log ('req.user.toObject()', req.user.toObject());
     res.render('profile', {
         title: 'Profil user',
         isProfile: true,
@@ -13,6 +15,24 @@ router.get('/', async (req, res) => {
     })
 })
 
-router.post('/', async () => {});
+router.post('/', async (req, res) => {
+    console.log('req.body ', req.body);
+    try {
+        const user = await User.findById(req.user._id);
+        const toChange = {
+            name : req.body.name
+        }
+        console.log('req.file ----', req.file);
+        if(req.file) {
+            toChange.avatarUrl = req.file.path;
+        }
+
+        Object.assign(user, toChange);
+        await user.save();
+        res.redirect('/profile');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 module.exports = router;
